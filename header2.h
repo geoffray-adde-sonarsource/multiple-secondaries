831ef3706cd3bac35f1958d3b339944b9915cdca
Geoffray Adde <geoffray.adde@sonarsource.com>
2023-10-16 20:07:23 +0200

    cleanup and make different implementations of end functions

--- header2.h
+++ header2.h
@@ -7,3 +7,11 @@ int f2a(int n, int p ) {
 int f2b(int n) {
     return n;
 }
+
+int g2a(int n, int p ) {
+    return n*n/p;
+}
+
+int g2b(int n) {
+    return n;
+}
--- issue1.cpp
+++ issue1.cpp
@@ -19,7 +19,7 @@ int f(int n, int p) {
 
 int g(int n, int p) {
     if(p==0) {
-        return ni_f1(n,p); // div by 0 in non-indexed files
+        return g1(n,p); // div by 0 in non-indexed files
     } 
     else {
         return 0;
