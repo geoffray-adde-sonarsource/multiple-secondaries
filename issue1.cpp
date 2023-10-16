0d4a8fc9c8f368c725ba370b3cf5d161c3d5d866
Geoffray Adde <geoffray.adde@sonarsource.com>
2023-10-16 19:34:41 +0200

    introduce weird header

--- issue1.cpp
+++ issue1.cpp
@@ -1,9 +1,20 @@
 #include "header1.h"
+#include "header1.weird"
 
 int f(int n, int p) {
 
     if(p==0) {
-        return f1(n,p);
+        return f1(n,p); // div by 0 in indexed files
+    } 
+    else {
+        return 0;
+    }
+}
+
+int f(int n, int p) {
+
+    if(p==0) {
+        return ni_f1(n,p); // div by 0 in non-indexed files
     } 
     else {
         return 0;
